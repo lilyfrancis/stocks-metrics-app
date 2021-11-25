7a7d670c9150cc1530a2901d81c659d405001ed8
Lilian Francis <ifnotlily@yahoo.com>
2021-11-25 07:31:50 +0100

    Modify the react index file

--- src/index.js
+++ src/index.js
@@ -6,4 +6,13 @@ import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
 
-
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
